09ada4a2090cac5a00bf201c04034b2b140a7390
Veltin Dupont <veltind@gmail.com>
2017-03-12 00:28:44 +0100

    Removing pandas dependency

--- deepforest/utils.py
+++ deepforest/utils.py
@@ -1,7 +1,3 @@
-import numpy as np
-import pandas as pd
-
-
 def check_models(models):
     at_least_one_model = False
     for model in models:
@@ -16,11 +12,3 @@ def check_model(model):
     has_predict_method = hasattr(model, "predict")
     assert has_fit_method and has_predict_method, \
         "models should have fit and predict methods"
-
-
-def predictions_to_dataframe(predictions, index, n_models):
-    n_class = predictions[0].shape[1]
-    columns = ["model{}_{}".format(model_number, class_number) for model_number
-               in range(n_models) for class_number in range(n_class)]
-    concat_dataframe = np.concatenate(predictions, axis=1)
-    return pd.DataFrame(concat_dataframe, columns=columns, index=index)
--- tests/tests_utils.py
+++ tests/tests_utils.py
@@ -1,24 +1,9 @@
-import numpy as np
-import pandas as pd
 import pytest
 
-from deepforest.utils import predictions_to_dataframe, check_model
+from deepforest.utils import check_model
 
 
 class TestUtils(object):
-    def test_predictions_to_dataframe_should_convert_list_of_array_to_dataframe(
-            self):
-        # Given
-        n_models = 3
-        arrays = [np.random.rand(2, 4) for i in range(n_models)]
-
-        # When
-        dataframe = predictions_to_dataframe(arrays, (0, 1), n_models)
-
-        # Check
-        assert isinstance(dataframe, pd.DataFrame)
-        assert dataframe.shape == (2, 12)
-
     def test_check_model_should_raise_error_if_object_has_no_fit_method(self):
         # Given
         class StubPredict:
@@ -32,7 +17,8 @@ class TestUtils(object):
             # When
             check_model(stubPredict)
 
-    def test_check_model_should_raise_error_if_object_has_no_predict_method(self):
+    def test_check_model_should_raise_error_if_object_has_no_predict_method(
+            self):
         # Given
         class StubFit:
             def fit(self):
