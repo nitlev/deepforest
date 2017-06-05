0dfff97476d3a8b0c44ce2ac658ce554948cef4d
Veltin Dupont <veltind@gmail.com>
2017-06-05 18:39:09 +0200

    scanning now returns new array of tree-transformed data

--- deepforest/scanning.py
+++ deepforest/scanning.py
@@ -1,9 +1,34 @@
 import numpy as np
+from sklearn.feature_extraction.image import extract_patches_2d
+
+from deepforest.models import Models
 
 
 class Scanning(object):
-    def __init__(self):
-        pass
+    def __init__(self, models, n_patch, patch_size=(2, 2)):
+        self.models = Models(models)
+        self.n_patch = n_patch
+        self.patch_size = patch_size
 
     def fit_transform(self, X, y):
-        return np.array([[]])
+        transformed_X, transformed_y = self._scan(X, y)
+        self.models.fit(transformed_X, transformed_y)
+        models_predictions = self.models.predict_proba(transformed_X)
+        return models_predictions.reshape(len(X), -1)
+
+    def _scan(self, X, y=None):
+        arrays, n_patches = zip(*(self._patch(x) for x in X))
+        new_X = np.vstack(arrays)
+
+        if y is not None:
+            new_y = np.repeat(y, n_patches, axis=0)
+            return new_X, new_y
+        else:
+            return new_X
+
+    def _patch(self, X):
+        patches = extract_patches_2d(X, self.patch_size,
+                                     max_patches=self.n_patch)
+        lines = (np.hstack(np.hstack(x)) for x in patches)
+        array = np.vstack(x for x in lines)
+        return array, len(patches)
--- tests/tests_scanning.py
+++ tests/tests_scanning.py
@@ -1,13 +1,20 @@
 import numpy as np
 
 from deepforest.scanning import Scanning
-from .utils import TestWithData
+from .utils import TestWithData, create_models, prepare_x, prepare_y
 
 
 class Test(TestWithData):
-    def test_scanning_fit_transform_should_return_2_dim_array(self):
+    def setup(self):
+        self.X_train = prepare_x((100, 3, 3))
+        self.y_train = prepare_y((100,))
+
+    def test_fit_transform_should_return_2_dim_array(self):
         # Given
-        scanning = Scanning()
+        n_models = 2
+        n_patches = 3
+        models = create_models(n=n_models, predicted_value=self.y_train)
+        scanning = Scanning(models, n_patches, patch_size=(2, 2))
 
         # When
         result = scanning.fit_transform(self.X_train, self.y_train)
@@ -15,3 +22,60 @@ class Test(TestWithData):
         # Check
         assert isinstance(result, np.ndarray)
         assert len(result.shape) == 2
+
+    def test_fit_transform_should_return_array_with_proper_dimensions(self):
+        # Given
+        n_classes = 2
+        n_models = 2
+        n_patches = 3
+        models = create_models(n=n_models,
+                               predicted_value=np.repeat(self.y_train,
+                                                         n_patches, axis=0))
+        scanning = Scanning(models, n_patches, patch_size=(2, 2))
+
+        # When
+        result = scanning.fit_transform(self.X_train, self.y_train)
+
+        # Check
+        assert result.shape[1] == n_classes * n_patches * n_models
+
+    def test_scan_should_return_properly_formatted_array(self):
+        # Given
+        n_models = 2
+        n_patches = 3
+        patch_size = 2
+        models = create_models(n=n_models, predicted_value=self.y_train)
+        scanning = Scanning(models, n_patches,
+                            patch_size=(patch_size, patch_size))
+
+        # When
+        result = scanning._scan(self.X_train)
+
+        # Check
+        assert isinstance(result, np.ndarray)
+        assert len(result.shape) == 2
+        assert result.shape == (len(self.X_train) * n_patches,
+                                patch_size * patch_size)
+
+    def test_scan_with_y_parameter_should_return_two_properly_sized_arrays(self):
+        # Given
+        n_models = 2
+        n_patches = 3
+        patch_size = 2
+        models = create_models(n=n_models, predicted_value=self.y_train)
+        scanning = Scanning(models, n_patches,
+                            patch_size=(patch_size, patch_size))
+
+        # When
+        result_x, result_y = scanning._scan(self.X_train, self.y_train)
+
+        # Check
+        assert isinstance(result_x, np.ndarray)
+        assert len(result_x.shape) == 2
+        assert result_x.shape == (len(self.X_train) * n_patches,
+                                  patch_size * patch_size)
+
+        # And
+        assert isinstance(result_y, np.ndarray)
+        assert len(result_y.shape) == len(self.y_train.shape)
+        assert result_y.shape[0] == len(self.X_train) * n_patches
